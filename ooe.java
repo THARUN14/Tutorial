df055249477cb0945a8850c71d3bb4292a4a8993
THARUN14 <tharun.muthusamy@gmail.com>
2017-04-10 20:06:26 +0530

    this my java prog
    positive or negative

---
+++ ooe.java
@@ -0,0 +1,18 @@
+class ooe
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+if(a==0)
+{
+System.out.print("Number is zero");
+}
+else if(a>0)
+{
+System.out.print("Number is positive");
+}
+else
+{
+System.out.print("Number is negative");
+}
+}
